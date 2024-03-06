af2a4a978e6d5d3989c9ad7557fcc4f1e956a09a
Aniki <anikipelau@gmail.com>
2024-03-07 02:32:19 +0300

    add simple example

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/trueaniki/admiral"
 )
 
+type ListenSubcommand struct {
+	Host string `type:"flag" name:"host" alias:"h" description:"Host to listen on" required:"true"`
+	Port int    `type:"flag" name:"port" alias:"p" description:"Port to listen on" default:"8080" required:"true"`
+}
+
+type Conf struct {
+	Host   string           `type:"flag" name:"host" alias:"h" description:"Host to listen on" required:"true"`
+	Port   int              `type:"flag" name:"port" alias:"p" description:"Port to listen on" default:"8080" required:"true"`
+	Listen ListenSubcommand `type:"command" name:"listen" description:"Listen specified host and port"`
+}
+
 func main() {
-	a := admiral.New("example", "example app")
-	a.AddCommand("hello", "say hello").AddCommand("world", "say world").AddFlag("name", "n", "your name")
-	a.Command("hello").Command("world").AddFlag("namename", "n", "your name")
-	a.Command("hello").Command("world").AddFlag("namenamename", "n", "your name")
-	a.Command("hello").Command("world").AddFlag("namenamenamename", "n", "your name")
-	a.Command("hello").Command("world").AddFlag("namenamenamenamename", "n", "your name")
-	fmt.Println(a.Command("hello").Command("world").Help())
+	a := admiral.New("example", "Example app")
+	conf := Conf{}
+	a.Configure(&conf)
+	a.Parse(os.Args)
+	fmt.Printf("%+v\n", conf)
+	fmt.Println(a.Help())
 }
